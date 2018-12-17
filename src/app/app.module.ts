a2e4ca521c9dea726d5f9dfffad044d4a11b3929
AndreaGB <andreagbretones@gmail.com>
2018-12-17 12:50:57 +0100

    component main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { StickyMenuComponent } from './sticky-menu/sticky-menu.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StickyMenuComponent
+    StickyMenuComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
