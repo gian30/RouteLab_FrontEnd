f6d10cd69c5807d9b2219310bdf3164f05276ef8
AndreaGB <andreagbretones@gmail.com>
2018-12-17 12:35:35 +0100

    Sticky menu

---
+++ src/app/sticky-menu/sticky-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sticky-menu',
+  templateUrl: './sticky-menu.component.html',
+  styleUrls: ['./sticky-menu.component.css']
+})
+export class StickyMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
