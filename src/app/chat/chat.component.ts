88ae95ea6114fa69236397aeb4b8ff34be9208d8
System Administrator <gianluca.ex.3007@gmail.com>
2019-01-16 11:44:46 +0100

    chat acabado

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,16 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, AfterViewChecked, ElementRef, ViewChild, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewChecked {
 
   constructor() {
   }
 
-  opened = false;
+  opened = true;
+  openedUser = true;
 
   userselected = {
     username : '',
@@ -18,11 +19,29 @@ export class ChatComponent implements OnInit {
   };
 
 
+  @ViewChild('scrollMe') private myScrollContainer: ElementRef;
+
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
+    } catch(err) { }
+  }
+
+  openUser() {
+    this.openedUser = !this.openedUser;
+  }
+
   openChat() {
     this.opened = !this.opened;
   }
 
   ngOnInit() {
+    this.scrollToBottom();
   }
 
 }
