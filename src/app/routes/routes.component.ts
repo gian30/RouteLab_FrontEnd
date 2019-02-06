68a13c22d76d487c6532357a10a93b6ff7cb8570
Andrea <andreagbretones@gmail.com>
2019-02-06 13:20:29 +0100

    gallery

--- src/app/routes/routes.component.ts
+++ src/app/routes/routes.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./routes.component.css']
 })
 export class RoutesComponent implements OnInit {
-  private ROUTE_SAMPLE = ("../../assets/img/sample_route.png");
-  private LOCATION = ("../../assets/icons/location.png");
-  private STAR = ("../../assets/icons/star.png");
-  private TIMER = ("../../assets/icons/timer.png");
+  private ROUTE_SAMPLE = ('../../assets/img/sample_route.png');
+  private LOCATION = ('../../assets/icons/location.png');
+  private STAR = ('../../assets/icons/star.png');
+  private TIMER = ('../../assets/icons/timer.png');
   loadRoutes = true;
   @Input() titleRoutes: string;
 
