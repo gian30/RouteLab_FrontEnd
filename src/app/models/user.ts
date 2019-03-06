687b889d7b6a8e04179e154c8e620e80c5858388
gian30 <gianlucalvs@gmail.com>
2019-03-06 11:49:45 +0100

    user class ended

---
+++ src/app/models/user.ts
@@ -0,0 +1,10 @@
+class User {
+  private userID: number;
+  private userName: string;
+  private userUsername: string;
+  private userEmail: string;
+  private userBirth: string;
+  private userLocality: string;
+  private userAdmin: boolean;
+  private userEmpresa: string;
+}
