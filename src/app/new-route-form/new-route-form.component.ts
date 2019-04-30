81b5231c6b30a053a3219c1995053981b649bb53
Andrea <andreagbretones@gmail.com>
2019-04-30 12:32:46 +0200

    Upload a new route loading from Data Base info (completed), except from the images

--- src/app/new-route-form/new-route-form.component.ts
+++ src/app/new-route-form/new-route-form.component.ts
@@ -1,31 +1,52 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, FormArray, FormControl} from '@angular/forms';
 import {stringify} from 'querystring';
+import {Address} from 'ngx-google-places-autocomplete/objects/address';
+import {ActivatedRoute} from '@angular/router';
+import {PostService} from '../services/post.service';
+
 
 @Component({
   selector: 'app-new-route-form',
   templateUrl: './new-route-form.component.html',
-  styleUrls: ['./new-route-form.component.css']
+  styleUrls: ['./new-route-form.component.css'],
+  providers: [PostService]
 })
 export class NewRouteFormComponent implements OnInit {
-  category = 'Categoría';
-  categories = ['Sol y playa', 'Deportivo', 'Naturaleza', 'De montaña', 'Histórico', 'Aventura', 'Rural', 'Científico'];
+  recomendation = 'Recomendaciones';
+  recomendations = ['Ropa de abrigo', 'Calzado cómodo', 'Agua', 'Comida', 'Ropa de baño'];
+  fullAddress = {};
+  private id = this.route.snapshot.paramMap.get('id');
 
-
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private route: ActivatedRoute, private _postService: PostService) {
   }
 
-  public message = 'Título necesario.';
+  // public message = 'Título necesario.';
 
 
   productForm: FormGroup;
 
+
+
+
+  /*Google Maps API*/
+  public handleAddressChange(address: Address) {
+    this.fullAddress = {
+      poblacion: address.address_components[0]['long_name'],
+      pais: address.address_components[3]['long_name'],
+      direccion: address.formatted_address,
+      latitud: address.geometry.location.lat(),
+      longitud: address.geometry.location.lng()
+    };
+  }
   ngOnInit() {
     /* Initiate the form structure */
     this.productForm = this.fb.group({
       origen: [],
       destino: [],
       select: [],
+      description: [],
+
 
 
     });
@@ -38,8 +59,25 @@ export class NewRouteFormComponent implements OnInit {
     // asignBlack() cambia el color de 'categoria' una vez seleccionda una opcion
 
   }
+   uploadRoute() {
+    console.log(this.productForm);
+  }
+
   assignBlack() {
     const element = document.getElementById('addBlack');
     element.classList.add('black');
+  }
+  loadRecomendaciones() {
+    this._postService.getRecomendacion(this.id).subscribe(
+      resul => {
+        if (resul.body !== null) {
+          this.recomendations = resul.body['data'];
+          console.log(this.recomendations);
+        }
+      }, error => {
+        console.log(error);
+
+      }
+    );
   }
 }
